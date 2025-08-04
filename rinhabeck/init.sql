f13003a58014f782c419ad75c8b27eb791918cec
arthurd3 <arthur_camposl@yahoo.com>
2025-08-04 10:32:15 -0300

    adjust code

--- rinhabeck/init.sql
+++ rinhabeck/init.sql
@@ -1,8 +1,12 @@
-CREATE UNLOGGED TABLE payments (
-    correlationId UUID PRIMARY KEY,
-    amount DECIMAL NOT NULL,
-    requested_at TIMESTAMP NOT NULL,
-    processor_type VARCHAR(10) NOT NULL
+CREATE TABLE IF NOT EXISTS payments (
+    id SERIAL PRIMARY KEY,
+    payment_id VARCHAR(255) NOT NULL,
+    amount INTEGER NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    description TEXT,
+    processed_amount INTEGER NOT NULL,
+    processor_type VARCHAR(20) NOT NULL DEFAULT 'default'
 );
 
-CREATE INDEX idx_payments_requested_at_processor ON payments (requested_at, processor_type);
+CREATE INDEX IF NOT EXISTS idx_payments_processor_type ON payments(processor_type);
+CREATE INDEX IF NOT EXISTS idx_payments_payment_id ON payments(payment_id);
